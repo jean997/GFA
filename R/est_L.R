f4a9d3710e117a93e55bb3b1228a9fb3304d8d8b
Jean Morrison <jeanm@uchicago.edu>
2020-10-04 15:16:48 -0400

    add ignore_tau and allow for different variance across snps

--- R/est_L.R
+++ R/est_L.R
@@ -1,5 +1,6 @@
 #'@export
-est_L <- function(B_hat, S_hat, N, adjust=TRUE, fit){
+est_L <- function(B_hat, S_hat, N, adjust=TRUE, fit, ignore_tau =TRUE){
+  stopifnot(ignore_tau)
   n_var <- nrow(B_hat)
   n_trait <- ncol(B_hat)
   n_factor <- ncol(fit$F_hat)
@@ -10,13 +11,18 @@ est_L <- function(B_hat, S_hat, N, adjust=TRUE, fit){
     B_tilde = B_hat
     S2_tilde = S_hat^2
   }
-  tau <- flashier:::get.tau(fit$fit$flash.fit)
-  T2_tilde = t( (1/tau) * t(matrix(1, nrow=n_var, ncol=n_trait)))
 
-  H <- solve(t(F_hat) %*% F_hat) %*% t(F_hat)
+  H <- with(fit, solve(t(F_hat) %*% F_hat) %*% t(F_hat))
   L_est <- H %*% t(B_hat) %>% t()
-  S <- diag(S2_tilde[1,] + T2_tilde[1,])
-  L_est_var <- H%*% S %*% t(H)
-  L_est_se <- t(sqrt(diag(L_est_var))*t(matrix(1, n_var,n_factor)))
+  if(ignore_tau){
+    L_est_var <- map(seq(nvar), function(i){
+                          V <- H%*% diag(S_hat[i,]) %*% t(H)
+                          sqrt(diag(V))
+                        }) %>%
+                  do.call(rbind, .)
+  }else{
+    tau <- flashier:::get.tau(fit$fit$flash.fit)
+    T2_tilde = t( (1/tau) * t(matrix(1, nrow=n_var, ncol=n_trait)))
+  }
   return(list(L_est = L_est, L_est_se = L_est_se))
 }
