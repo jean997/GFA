2c5b1c4add63ac733db8d4380ffe5c413b84deac
Jean Morrison <jeanm@uchicago.edu>
2020-10-05 15:07:40 -0400

    now just provide tau as a vector to not have to deal with different flashier outputs

--- R/est_L.R
+++ R/est_L.R
@@ -1,9 +1,15 @@
 #'@export
-est_L <- function(B_hat, S_hat, N, adjust=TRUE, fit, ignore_tau =TRUE){
-  stopifnot(ignore_tau)
+est_L <- function(B_hat, S_hat, N, tau, adjust=TRUE, fit, ignore_tau =FALSE){
+
   n_var <- nrow(B_hat)
   n_trait <- ncol(B_hat)
   n_factor <- ncol(fit$F_hat)
+  if(!ignore_tau){
+    if(missing(tau)){stop("Please provide tau.")}
+    stopifnot(length(tau)==n_trait)
+  }else{
+    tau <- rep(1, n_trait)
+  }
   if(adjust){
     B_tilde = t( (1/sqrt(N)) *t(B_hat/S_hat))
     S2_tilde = t( (1/N) * t(matrix(1, nrow=n_var, ncol=n_trait)))
@@ -14,15 +20,11 @@ est_L <- function(B_hat, S_hat, N, adjust=TRUE, fit, ignore_tau =TRUE){
 
   H <- with(fit, solve(t(F_hat) %*% F_hat) %*% t(F_hat))
   L_est <- H %*% t(B_hat) %>% t()
-  if(ignore_tau){
-    L_est_se <- map(seq(n_var), function(i){
-                          V <- H%*% diag(S2_tilde[i,]) %*% t(H)
-                          sqrt(diag(V))
-                        }) %>%
-                  do.call(rbind, .)
-  }else{
-    tau <- flashier:::get.tau(fit$fit$flash.fit)
-    T2_tilde = t( (1/tau) * t(matrix(1, nrow=n_var, ncol=n_trait)))
-  }
+  L_est_se <- map(seq(n_var), function(i){
+    V <- H%*% diag(S2_tilde[i,] + 1/tau) %*% t(H)
+    sqrt(diag(V))
+  }) %>%
+  do.call(rbind, .)
+
   return(list(L_est = L_est, L_est_se = L_est_se))
 }
