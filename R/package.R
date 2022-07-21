ccf3c84ec576564c38d1d413014d1b29e9c3961a
Jean Morrison <morrison.jeanv@gmail.com>
2022-07-21 19:59:24 -0400

    import lpSolve, update email address

--- R/package.R
+++ R/package.R
@@ -3,9 +3,9 @@
 #' Helper package for experimenting with factor decomposition of summary statistics.
 #'
 #' @docType package
-#' @author Jean Morrison <jeanm@uchicago.edu>
+#' @author Jean Morrison <jvmorr@umich.edu>
 #' @import flashier
-#' @import dplyr readr reshape2
+#' @import dplyr readr reshape2 lpSolve
 #' @import purrr ggplot2
 #' @import viridis
 #' @name sumstatFactors
