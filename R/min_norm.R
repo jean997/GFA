3a87de8bc3f1600cb1921f053cb1dc45f8113a5c
Jean Morrison <morrison.jeanv@gmail.com>
2022-07-25 17:05:48 -0400

    add evaluation for l

--- R/min_norm.R
+++ R/min_norm.R
@@ -1,5 +1,7 @@
 #'@export
-min_norm <- function(f_true, f_hat, single_trait_thresh = 0.95, thresh = 0, return_Q = FALSE){
+min_norm <- function(f_true, f_hat, l_true, l_hat,
+                     single_trait_thresh = 0.95, thresh = 0,
+                     return_Q = FALSE){
 
   M <- nrow(f_true)
   stopifnot(nrow(f_hat) == M)
@@ -28,13 +30,32 @@ min_norm <- function(f_true, f_hat, single_trait_thresh = 0.95, thresh = 0, retu
     f_hat <- f_hat[,-i]
     hat_ix <- hat_ix[-i]
   }
+
+  if(!missing(l_true) & !missing(l_hat)){
+    stopifnot(ncol(l_true) == n_t & ncol(l_hat) == n_h)
+    N <- nrow(l_true)
+    stopifnot(nrow(l_hat) == N)
+    l_true <- norm_cols(l_true)$A
+    l_hat <- norm_cols(l_hat)$A
+    l_true <- l_true[,true_ix]
+    l_hat <- l_hat[,hat_ix]
+    l <- TRUE
+  }
+
+
   k <- ncol(f_true) - ncol(f_hat)
   if(k > 0){
     f_hat <- cbind(f_hat, matrix(0, nrow = M, ncol = k))
     hat_ix <- c(hat_ix, seq(k) + n_h)
+    if(l){
+      l_hat <- cbind(l_hat, matrix(0, nrow = N, ncol = k))
+    }
   }else if(k < 0){
     f_true <- cbind(f_true, matrix(0, nrow = M, ncol = -k))
     true_ix <- c(true_ix, seq(k) + n_t)
+    if(l){
+      l_true <- cbind(l_true, matrix(0, nrow = N, ncol = -k))
+    }
   }
 
 
@@ -54,6 +75,9 @@ min_norm <- function(f_true, f_hat, single_trait_thresh = 0.95, thresh = 0, retu
   Q <- t(b$solution*sgn)
 
   frob_n <- sum((f_true - f_hat%*%Q)^2)
+  if(l){
+    frob_n_l <- sum((l_true - l_hat%*%Q)^2)
+  }
 
   n <- ncol(d)
 
@@ -74,10 +98,12 @@ min_norm <- function(f_true, f_hat, single_trait_thresh = 0.95, thresh = 0, retu
   if(return_Q){
     ret$Q <- Q
   }
+  if(l){
+    ret$frob_n_l <- frob_n_l
+  }
   return(ret)
-
-
 }
+
 
 
 norm_cols <- function(A){
