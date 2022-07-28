fd8210bdb85feb521cf6b538d55df03f3c40ab0d
Jean Morrison <morrison.jeanv@gmail.com>
2022-07-28 19:16:31 -0400

    add back simulation function generate_F_simple

--- R/sim_sumstats_lf.R
+++ R/sim_sumstats_lf.R
@@ -345,3 +345,28 @@ sim_ld_prune <- function(dat, pvalue, R_LD, r2_thresh = 0.1, pval_thresh = 1){
     }) %>% unlist()
   return(keep_list)
 }
+
+
+#'@export
+generate_F_simple <- function(nblocks, type=c("nested", "difference",
+                                              "checkers1", "checkers2")){
+
+  type <- match.arg(type)
+  if(type == "nested"){
+    m <-  cbind(c(1,1),
+                c(1, 1),
+                c(0, 0))
+  }else if(type == "difference"){
+    m <- cbind(c(1, 1, 1), c(1, -1, 0))
+  }else if(type == "checkers1"){
+    m <- cbind(c(1, 1, 1),
+               c(1, 1, 0 ),
+               c(0, 1, 1))
+  }else if(type == "checkers2"){
+    m <-  cbind(c(1, 0, 1),
+                c(1, 1, 0 ),
+                c(0, 1, 1))
+  }
+  F_mat <- kronecker(diag(nblocks), m)
+  return(F_mat)
+}
