60cfd33354cc1a233cf0735b90e3d8bb29948584
Jean Morrison <jeanm@uchicago.edu>
2023-10-11 15:54:13 -0400

    add sample size to pve by trait

--- R/pve_by_trait.R
+++ R/pve_by_trait.R
@@ -1,18 +1,24 @@
 
 # Should return a matrix that is traits by factors
-pve_by_trait <- function(Lhat, Fhat, tau){
+pve_by_trait <- function(Lhat, Fhat, tau, sample_size){
   nvar <- nrow(Lhat)
   ntrait <- nrow(Fhat)
   k <- ncol(Lhat)
   stopifnot(ncol(Fhat) == k)
-  if(class(tau)=="numeric") tau <- matrix(tau, nrow=nvar, ncol=ntrait)
+  if(missing(sample_size)) sample_size <- rep(1, ntrait)
+  stopifnot(length(sample_size) == ntrait)
+  if(class(tau)=="numeric") tau <- matrix(tau, nrow=nvar, ncol=ntrait, byrow = TRUE)
   stopifnot(nrow(tau) == nvar & ncol(tau) == ntrait)
+  # variance explained per factor
   sj <- purrr::map_dfc(seq(k), function(kk){
     B <- Lhat[,kk] %*% t(Fhat[,kk])
-    colSums(B^2)
+    B_e <- B/sqrt(sample_size)
+    colSums(B_e^2)
   })
+  tau <- t(t(tau)*sample_size)
   tau_sums <- colSums(1/tau)
   s_sums <- rowSums(sj)
-  sums <- matrix(rep(s_sums + tau_sums, k), byrow = F)
+  sums <- s_sums + tau_sums
   pve_j <- sj/sums
 }
+
