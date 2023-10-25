bc6f79546a03d56b8baab8960df7109230cea363
Jean Morrison <morrison.jeanv@gmail.com>
2023-10-25 10:54:44 -0400

    add loadings_gls

---
+++ R/loadings_gls.R
@@ -0,0 +1,32 @@
+loadings_gls <- function(X, S, R, F_hat){
+  ntrait <- ncol(X)
+  nvar <- nrow(X)
+  nfactor <- ncol(F_hat)
+  stopifnot(nrow(F_hat) == ntrait)
+  stopifnot(nrow(S) == nvar & ncol(S) == ntrait)
+
+  if(missing(R)){
+    R <- diag(ntrait, nrow = ntrait)
+  }
+  stopifnot(nrow(R) == ntrait & ncol(R) == ntrait)
+  stopifnot(isSymmetric(R))
+  stopifnot(all(eigen(R)$values > 0))
+
+  Rinv <- solve(R)
+  if(all(S == 1)){
+    ftf_inv <- solve(t(F_hat) %*% Rinv %*% F_hat)
+    ftx <- t(F_hat) %*% Rinv %*% t(X)
+    L <- t(ftf_inv %*% ftx)
+    S <- t(matrix(1, nrow = nfactor, ncol = nvar) * sqrt(diag(ftf_inv)))
+  }else{
+    LS <- sapply(1:nvar, function(i){
+      s <- S[i,]
+      ftf_inv <- solve(t(F_hat) %*% diag(1/s) %*% Rinv %*% diag(1/s) %*% F_hat)
+      ftx <- t(F_hat) %*% diag(1/s) %*% Rinv %*% diag(1/s) %*% t(X[i,,drop=F])
+      c(ftf_inv %*% ftx, sqrt(diag(ftf_inv)))
+    }) %>% matrix(nrow = nvar, byrow = TRUE)
+    L <- L[, 1:nvar]
+    S <- L[, -(1:nvar)]
+  }
+  return(list("L" = L, "S" = S))
+}
