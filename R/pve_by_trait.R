8e2a2142a2257c8d46ff233fbaa82308137ed892
Jean Morrison <morrison.jeanv@gmail.com>
2023-10-28 20:59:06 -0400

    woops row multiply

--- R/pve_by_trait.R
+++ R/pve_by_trait.R
@@ -11,13 +11,13 @@ pve_by_trait <- function(Lhat, Fhat, tau, sample_size){
   stopifnot(nrow(tau) == nvar & ncol(tau) == ntrait)
   # variance explained per factor
   sj <- purrr::map_dfc(seq(k), function(kk){
-    B <- Lhat[,kk] %*% t(Fhat[,kk])
-    B_e <- B/sqrt(sample_size)
+    B <- Lhat[,kk] %*% t(Fhat[,kk]) ## total effects on original (usually z-score scale)
+    B_e <- t(t(B)/sqrt(sample_size)) ## standardized effect scale effects
     colSums(B_e^2)
   })
   tau <- t(t(tau)*sample_size)
-  tau_sums <- colSums(1/tau)
-  s_sums <- rowSums(sj)
+  tau_sums <- colSums(1/tau) # per-trait variance explained by error
+  s_sums <- rowSums(sj) # per-trait variance explained by factors
   sums <- s_sums + tau_sums
   pve_j <- sj/sums
 }
