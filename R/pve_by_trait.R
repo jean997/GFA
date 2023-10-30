80d46026eeea0995077a8236a7aaab86957b2990
Jean Morrison <morrison.jeanv@gmail.com>
2023-10-30 12:13:00 -0400

    fix pve calculation

--- R/pve_by_trait.R
+++ R/pve_by_trait.R
@@ -1,24 +1,49 @@
 
 # Should return a matrix that is traits by factors
-pve_by_trait <- function(Lhat, Fhat, tau, sample_size){
-  nvar <- nrow(Lhat)
-  ntrait <- nrow(Fhat)
-  k <- ncol(Lhat)
-  stopifnot(ncol(Fhat) == k)
+pve_by_trait <- function(fit, sample_size){
+  nvar <- nrow(fit$L_hat)
+  ntrait <- nrow(fit$F_hat)
+
   if(missing(sample_size)) sample_size <- rep(1, ntrait)
   stopifnot(length(sample_size) == ntrait)
-  if(class(tau)=="numeric") tau <- matrix(tau, nrow=nvar, ncol=ntrait, byrow = TRUE)
-  stopifnot(nrow(tau) == nvar & ncol(tau) == ntrait)
-  # variance explained per factor
-  sj <- purrr::map_dfc(seq(k), function(kk){
-    B <- Lhat[,kk] %*% t(Fhat[,kk]) ## total effects on original (usually z-score scale)
-    B_e <- t(t(B)/sqrt(sample_size)) ## standardized effect scale effects
-    colSums(B_e^2)
-  })
-  tau <- t(t(tau)*sample_size)
-  tau_sums <- colSums(1/tau) # per-trait variance explained by error
-  s_sums <- rowSums(sj) # per-trait variance explained by factors
-  sums <- s_sums + tau_sums
-  pve_j <- sj/sums
+
+  ef2 <- flashier:::get.EF2(fit$fit$flash_fit)
+  nf <- ncol(ef2[[1]])
+  est_ix <- setdiff(seq(nf), fit$fixed_ix) |> sort()
+
+  # variance explained per trait/factor
+  sj <- sapply(est_ix, function(kk){
+    Vk <- ef2[[1]][,kk] %*% t(ef2[[2]][,kk]) ## total effects on original (usually z-score scale)
+     ## standardized effect scale total variance
+    colSums(Vk)/sample_size
+  }) |> matrix(nrow = ntrait, byrow = FALSE)
+
+  # variance from fixed factors (this is part of error)
+  fj <- sapply(fit$fixed_ix, function(kk){
+    Vk <- ef2[[1]][,kk] %*% t(ef2[[2]][,kk]) ## total effects on original (usually z-score scale)
+    ## standardized effect scale total variance
+    colSums(Vk)/sample_size
+  })|> matrix(nrow = ntrait, byrow = FALSE)
+
+  tau <- flashier:::get.tau(fit$fit$flash_fit)
+  stopifnot(length(tau) == ntrait)
+  var_from_tau <- nvar/tau
+  var_from_tau <- var_from_tau/sample_size
+
+  tot_by_trait <- rowSums(sj) + rowSums(fj) + var_from_tau
+
+  # error component of tau
+  fixed_tau <- flash_fit_get_fixed_tau(fit$fit$flash_fit)
+
+  error_var <- rowSums(fj) + (nvar/fixed_tau)/sample_size
+
+  genet_var = tot_by_trait - error_var
+
+  pve_j <- sj/genet_var
+
+  return(list(h2 = genet_var, error_var = error_var, pve = pve_j))
 }
+
+
+
 
