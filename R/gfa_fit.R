d52900979de2ec464a5a5f9a787cfb3a64a65e92
Jean Morrison <jvmorr@umich.edu>
2025-09-01 22:12:30 -0400

    Remove check.arg for mode
    Removed unnecessary assignment of mode variable.

--- R/gfa_fit.R
+++ R/gfa_fit.R
@@ -84,7 +84,7 @@ gfa_fit <- function(Z_hat = NULL,
   for(n in names(params)){
     if(! n %in% names(default_params)) stop("Unknown parameter ", n, " provided.")
   }
-  mode <- match.arg(mode)
+  
   ## process inputs
   check_args_znbs(is.null(Z_hat), is.null(N), is.null(B_hat), is.null(S))
   if(is.null(Z_hat)){
