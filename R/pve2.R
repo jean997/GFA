6c43a4538605bdcf883b626a075b0cedb867d31d
Jean Morrison <jeanm@uchicago.edu>
2025-11-21 16:58:06 -0500

    refactor pve code

--- R/pve2.R
+++ R/pve2.R
@@ -7,91 +7,103 @@ pve2 <- function(gfa_fit){
   p <- nrow(ef2[[2]])
   n <- nrow(ef2[[1]])
 
+  tau <- flashier:::get.tau(gfa_fit$fit$flash_fit)
+  if(inherits(tau, "matrix")){
+    stopifnot(nrow(tau) == n & ncol(tau) == p) # should never fail
+    S2 <- gfa_fit$fit$flash_fit$given.S2
+    gvar_theta <- (colSums(1/tau) - colSums(S2))/gfa_fit$scale^2
+  }else{
+    stopifnot(length(tau) == p) # this should never fail
+    # error component of tau
+    fixed_tau <- flash_fit_get_fixed_tau(gfa_fit$fit$flash_fit)
+    est_tau <- 1/((1/tau) - (1/fixed_tau))
+    #error_var <-  (n/fixed_tau)/(gfa_fit$scale^2)
+    gvar_theta <- (n/est_tau)/(gfa_fit$scale^2)
+  }
+
+  est_ix <- 1:k
   if(gfa_fit$method == "fixed_factors"){
     fixed_ix <- which(gfa_fit$fit$flash_fit$fix.dim %>% sapply(., function(x){!is.null(x)}))
-    est_ix <- 1:gfa_fit$fit$n_factors
     if(length(fixed_ix) > 0){
       est_ix <- est_ix[-fixed_ix]
     }
-    #cat("est_ix:", est_ix, "\n")
-    # relative variance explained per trait/factor
-    sj <- sapply(est_ix, function(kk){
-      Vk <- flashier:::lowrank.expand(list(ef2[[1]][,kk,drop = F], ef2[[2]][,kk, drop = F])) ## total effects on z-score scale
-      ## standardized effect scale total variance
-      colSums(Vk)/(gfa_fit$scale^2)
-    }) |> matrix(nrow = p, byrow = FALSE)
-
-    # variance from fixed factors (this is part of error)
-    if(length(fixed_ix) == 0){
-      fj_sums <- rep(0, p)
-    }else{
-      fj_sums <- sapply(fixed_ix, function(kk){
-        Vk <- flashier:::lowrank.expand(list(ef2[[1]][,kk,drop = F], ef2[[2]][,kk, drop = F]))
-        ## standardized effect scale total variance
-        colSums(Vk)/(gfa_fit$scale^2)
-      })|> matrix(nrow = p, byrow = FALSE) |> rowSums()
-    }
-
-    tau <- flashier:::get.tau(gfa_fit$fit$flash_fit)
-    if(inherits(tau, "matrix")){
-      stopifnot(nrow(tau) == n & ncol(tau) == p) # should never fail
-      S2 <- gfa_fit$fit$flash_fit$given.S2
-      est_tau <- 1/((1/tau) - S2)
-    }else{
-      stopifnot(length(tau) == p) # this should never fail
-      # error component of tau
-      fixed_tau <- flash_fit_get_fixed_tau(gfa_fit$fit$flash_fit)
-      est_tau <- 1/((1/tau) - (1/fixed_tau))
-    }
-    error_var <- fj_sums + (n/fixed_tau)/(gfa_fit$scale^2)
-    total_var <- rowSums(sj) + fj_sums + (n/tau)/(gfa_fit$scale^2)
-
-    genet_var = rowSums(sj) + (n/est_tau)/(gfa_fit$scale^2)
-    pve_j <- sj/genet_var
-  }else if(gfa_fit$method == "random_effect"){
-    est_ix <- 1:k
-    # relative variance explained per trait/factor
-    sj <- sapply(est_ix, function(kk){
-      Vk <- flashier:::lowrank.expand(list(ef2[[1]][,kk,drop = F], ef2[[2]][,kk, drop = F])) ## total effects on z-score scale
-      ## standardized effect scale total variance
-      colSums(Vk)/(gfa_fit$scale^2)
-    }) |> matrix(nrow = p, byrow = FALSE)
-    est_tau <- flashier:::get.tau(gfa_fit$fit$flash_fit)
-    error_var <- colSums(gfa_fit$fit$flash_fit$EB2)
-    genet_var = rowSums(sj) + (n/est_tau)/(gfa_fit$scale^2)
-    pve_j <- sj/genet_var
-  }else if(gfa_fit$method == "noR"){
-    est_ix <- 1:k
-    # relative variance explained per trait/factor
-    sj <- sapply(est_ix, function(kk){
-      Vk <- flashier:::lowrank.expand(list(ef2[[1]][,kk,drop = F], ef2[[2]][,kk, drop = F])) ## total effects on z-score scale
-      ## standardized effect scale total variance
-      colSums(Vk)/(gfa_fit$scale^2)
-    }) |> matrix(nrow = p, byrow = FALSE)
-
-    tau <- flashier:::get.tau(gfa_fit$fit$flash_fit)
-    if(inherits(tau, "matrix")){
-      stopifnot(nrow(tau) == n & ncol(tau) == p) # should never fail
-      S2 <- gfa_fit$fit$flash_fit$given.S2
-      error_var <-  colSums(S2)/(gfa_fit$scale^2)
-      tau_var <- colSums(1/tau)
-      gvar_tau <- tau_var - error_var
-    }else{
-      stopifnot(length(tau) == p) # this should never fail
-      # error component of tau
-      fixed_tau <- flash_fit_get_fixed_tau(gfa_fit$fit$flash_fit)
-      est_tau <- 1/((1/tau) - (1/fixed_tau))
-      error_var <-  (n/fixed_tau)/(gfa_fit$scale^2)
-      gvar_tau <- (n/est_tau)/(gfa_fit$scale^2)
-    }
-    #total_var <- rowSums(sj) + (n/tau)/(gfa_fit$scale^2)
-
-    genet_var = rowSums(sj) + gvar_tau
-    pve_j <- sj/genet_var
   }
+
+  sj <- sapply(est_ix, function(kk){
+      Vk <- flashier:::lowrank.expand(list(ef2[[1]][,kk,drop = F], ef2[[2]][,kk, drop = F])) ## total effects on z-score scale
+      ## standardized effect scale total variance
+      colSums(Vk)/(gfa_fit$scale^2)
+  }) |> matrix(nrow = p, byrow = FALSE)
+
+  genet_var = rowSums(sj) + gvar_theta
+  pve_j <- sj/genet_var
+
   return(list(genet_var = genet_var, pve = pve_j))
 }
 
+### PVE calculation notes:
+#
+## We fit
+## Zhat = Bhatstd*S = L ( S F)^T + Theta S + E_s
+## where S is diagonal matrix of scales.
+## For continuous trait, the scale is sqrt(N)
+## Bhatstd gives effect estimates on the sd(Y)/sd(G) scale.
+##
+## For the noR case, elements of E_s are independent standard normal.
+## for the fixed factors case, E_s = Eprime + AW where W are fixed factors
+##
+## The genetic variance of trait j explained by factor k is
+##  sum_i (L_{i,k} F_{j,k})^2. The posterior expected value of this is given by
+# Vk <- flashier:::lowrank.expand(list(ef2[[1]][,k,drop = F], ef2[[2]][,k, drop = F])) ## total effects on z-score scale
+# colSums(Vk)/(gfa_fit$scale^2) ## This has length p
+##
+## The genetic variance explained by theta is stored in the tau object.
+## (1/tau) = colSums(theta_s^2)/n + colSums(Eprime^2)/n
+## colSums(Eprime^2)/n = 1/fixed_tau (= 1 noR or lambda_min for fixed_factors)
+## Therefore, the genetic variance explained by tau is
+## n((1/tau) - (1/fixed_tau)) = colSums(theta_s^2)
+## n((1/tau) - (1/fixed_tau))/scale^2 = colSums(theta^2)
+## This is given in gvar_theta
+## so total genetic variance for trait j is rowSums(sj) + gvar_theta
 
 
+pve_from_sample <- function(gfa_fit, samp){
 
+  k <- ncol(samp[["L"]])
+  p <- nrow(samp[["F"]])
+  n <- nrow(samp[["L"]])
+
+  tau <- flashier:::get.tau(gfa_fit$fit$flash_fit)
+  if(inherits(tau, "matrix")){
+    stopifnot(nrow(tau) == n & ncol(tau) == p) # should never fail
+    S2 <- gfa_fit$fit$flash_fit$given.S2
+    gvar_theta <- (colSums(1/tau) - colSums(S2))/gfa_fit$scale^2
+  }else{
+    stopifnot(length(tau) == p) # this should never fail
+    # error component of tau
+    fixed_tau <- flash_fit_get_fixed_tau(gfa_fit$fit$flash_fit)
+    est_tau <- 1/((1/tau) - (1/fixed_tau))
+    #error_var <-  (n/fixed_tau)/(gfa_fit$scale^2)
+    gvar_theta <- (n/est_tau)/(gfa_fit$scale^2)
+  }
+
+  est_ix <- 1:k
+  if(gfa_fit$method == "fixed_factors"){
+    fixed_ix <- which(gfa_fit$fit$flash_fit$fix.dim %>% sapply(., function(x){!is.null(x)}))
+    if(length(fixed_ix) > 0){
+      est_ix <- est_ix[-fixed_ix]
+    }
+  }
+
+  sj <- sapply(est_ix, function(kk){
+    Vk <- flashier:::lowrank.expand(list((samp[["L"]][,kk,drop = F])^2, (samp[["F"]][,kk, drop = F])^2)) ## total effects on z-score scale
+    ## standardized effect scale total variance
+    colSums(Vk)/(gfa_fit$scale^2)
+  }) |> matrix(nrow = p, byrow = FALSE)
+
+  genet_var = rowSums(sj) + gvar_theta
+  pve_j <- sj/genet_var
+
+
+  return(list(genet_var = genet_var, pve = pve_j))
+}
